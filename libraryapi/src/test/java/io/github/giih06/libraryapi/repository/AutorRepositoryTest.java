176773f24bd8d48d29d197e17b310fb610c5ab9a
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2025-04-27 16:10:30 -0300

    AutorRepositoryTest create

---
+++ libraryapi/src/test/java/io/github/giih06/libraryapi/repository/AutorRepositoryTest.java
@@ -0,0 +1,27 @@
+package io.github.giih06.libraryapi.repository;
+
+import io.github.giih06.libraryapi.model.Autor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class AutorRepositoryTest {
+
+    @Autowired
+    AutorRepository repository;
+
+    @Test
+    public void salvarTest(){
+        Autor autor = new Autor();
+        autor.setNome("Maria");
+        autor.setNacionalidade("Italiana");
+        autor.setDataNascimento(LocalDate.of(1950, 1, 31));
+
+        var autorSalvo = repository.save(autor);
+        System.out.println("Autor Salvo: " + autorSalvo);
+
+    }
+}
