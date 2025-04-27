727b818aa1ea59d6a7c0802a51d1e1825626b791
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2025-04-27 16:08:40 -0300

    model

---
+++ libraryapi/src/main/java/io/github/giih06/libraryapi/model/Livro.java
@@ -0,0 +1,40 @@
+package io.github.giih06.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data // è uma coposição de várias annotations como Getter, Setter, toString, EqualdAndHashCode e RequierdArgsConstructor
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private BigDecimal preco;
+
+    // chave estrangeira
+    @ManyToOne // Um autor pode ter muitos livros
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
